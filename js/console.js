a1a2455b231007605c1dbdbc2b9561775d037d76
Thomas <gonzo113@bassfreaks.de>
2020-12-18 19:51:43 +0100

    improved commenting in the code; minor improvements on autocomplete

--- js/console.js
+++ js/console.js
@@ -1,39 +1,95 @@
-window.onload = function(){
+// Ausführung wenn alles geladen wurde:
+document.addEventListener('DOMContentLoaded', function() {   // als Eventhandler, mit }); am Ende
+//window.onload = function(){                                            // als Funktion, mit } am Ende
 
     // EVENTHANDLER:
-    document.getElementById('consoleframe').addEventListener('click', setFocus);	// Focus bei Klick auf Fenster immer auf Befehlseingabefeld
-    var cmdin = document.getElementById("cmdin");                // Eingabefeld-Objekt
+    // =============
+
+    document.getElementById('consoleframe').addEventListener('click', setFocus);	// Focus bei Klick auf Konsolen-Fenster immer auf Befehlseingabefeld
     var cmd = document.getElementById('cmd');                    // Eingabeformular-Objekt
     cmd.addEventListener('keydown', keyhandler);                     // Eventhandler für Tastendruck
     cmd.addEventListener('keyup', shifthandler);                     // Eventhandler für das loslassen einer Taste (für den Fall dass SHIFT involviert ist)
     cmd.addEventListener('input', setinput);                         // Eventhandler für Änderung im Eingabefeld (vollständig, da auswertung nach Keyup)
 
-
     // GLOBALE VARIABLEN:
+    // ==================
+
+    // Links
+    //var links = document.getElementsByTagName("a");            // allgemeine Linkbehandlung  todo: non-URL-Links klicken ans laufen bringen
+    //links.onclick = openURL();
+    var linkelemente = document.getElementsByClassName("links"); // Array aller Links (mit der css-Klasse links) auf der Seite
+    var linknamen = [];                                                    // Array der Link-Texte, um diese als Befehl verwenden zu können
+    for (let i=0; i<linkelemente.length; i++) linknamen[i] = linkelemente[i].text.slice(1, linkelemente[i].text.length-1);  // Beschneidung derer um eckige Klammern zu entfernen
+    // Kommandos und Eingabe
+    var cmdin = document.getElementById("cmdin");                 // Eingabefeld-Objekt
     var commands = ["help", "about", "profile", "projects", "websites", "exit", "quit", "clear", "cls"];   // Liste möglicher Kommandos (muss mit entsprechenden node-IDs korrespondieren)
     commands.sort();
-    var prompts = ["$ ", "> ", "% ", ": ", "- ", "# "];                        // $: standard, #: root
-    var linkelemente = document.getElementsByClassName("links");    // Array aller Links auf der Seite
-    var linknamen = [];                                                        // Array der Link-Texte
-    for (let i=0; i<linkelemente.length; i++) linknamen[i] = linkelemente[i].text.slice(1, linkelemente[i].text.length-1);
-    var allcommands = commands.concat(linknamen);                              // Liste aller möglichen Eingabebefehle
-    var lastcommands = [];  // Liste zuletzt eingegebener Kommandos
-    var commandhistorypos = 0; // Stelle in der Befehlseingabenhistorie (mit Pfeil-rauf/-runter)
-    var shift = false;      // SHIFT gedrückt?
-    var autofillen = 0;     // Anzahl der Zeichen die ergänzt wurden
-    var tab = 0;            // Anzahl der Tabulator-drücke
-    var input = "";         // vollständige aktuelle Eingabe
-    //var command = "";       // vollständiges Kommando
+    //var prompts = ["$ ", "> ", "% ", ": ", "- ", "# "];                  // $: standard, #: root    todo: verschiedene Prompts implementieren, die je nach Kontext wechseln
+    var allcommands = commands.concat(linknamen);                          // Liste aller möglichen Eingabebefehle
+    var lastcommands = [];      // Liste zuletzt eingegebener Kommandos
+    var commandhistorypos = 0;  // Stelle in der Befehlseingabenhistorie (bei Benutzung von Pfeil-hoch/-runter)
+    var shift = false;          // SHIFT gedrückt?
+    var input = "";             // vollständige aktuelle Eingabe
+    var typedinput = "";        // vollständiges Kommando
+    // Autovervollständigung
+    var autofills = [];         // Array für gefundene Autovervollständigungen
+    var autofillPos = 0;        // Arrayposition der zuletzt gefundenen passenden Ergänzung zur Eingabe
+    var autofilLen = 0;         // Anzahl der Zeichen die ergänzt wurden
+    var tab = 0;                // Anzahl der Tabulator-drücke
+
 
     // FUNKTIONEN:
+    // ===========
 
-    // komplette Eingabe verfügbar machen
-    function setinput(e){
+    function setinput(e){       // komplette Eingabe in input-Variable verfügbar machen
         input = e.target.value;
     }
 
-    // Tastatureingabe auswerten
-    function keyhandler(key) {
+    function setFocus() {       // Focus auf das Eingabefeld setzen
+        document.getElementById("cmdin").focus();
+    }
+
+    function shifthandler(key){ // registrieren, ob Shift-Taste wieder losgelassen wurde
+        if(key.keyCode === 16){
+            shift = false;
+            console.log("SHIFT losgelassen");
+        }
+    }
+
+    // Funktionen für Keyhandler:
+
+    // Automatische Befehlsvervollständigung                        todo: ans funktionieren bekommen
+    function autocomplete(select){
+        typedinput = input;                                              // speichere aktuellen Inhalt des Eingafeldes zwischen
+        let inputlow = typedinput.toLowerCase();                         // Eingabefeld-Inhalt in Kleinbuchstaben
+        let inputlen = typedinput.length;                                // Textlänge des Eingabefeld-Inhalts
+        console.log("Autocomplete von " + inputlow);                     // prüfe ob Text in Eingabefeld mit Anfang eines Befehls übereinstimmt
+        autofills =  allcommands.filter(com => com.startsWith(inputlow));// finde alle Befehle, die mit den eingegebenen Buchstaben beginnen
+        console.log(autofills.length + " mögliche Befehle gefunden");
+        autofilLen = autofills[autofillPos].length - inputlen;           // speichere die Anzahl der Buchstaben der Ergänzung
+        document.getElementById("cmdin").value += autofills[autofillPos].slice(inputlen, autofills[autofillPos].length);
+        console.log("Ergänzung von " + autofillPos+1 + ". Befehl");
+        if (select) cmdin.setSelectionRange(inputlen, autofills[autofillPos].length, "backward"); // markiere die Ergänzung
+        if (shift) {                                                     // zähle die automatischen Ergänzungen rauf oder runter
+            if (autofillPos >= 0) autofillPos--;
+            else autofillPos = autofills.length-1;
+        } else {
+            if (autofillPos < autofills.length) autofillPos++;
+            else autofillPos = 0;
+        }
+    }
+
+    function autocompleteReset(){
+        typedinput = "";
+        autofills = null;
+        autofillPos = 0;
+        autofilLen = 0;
+        tab = 0;
+    }
+
+    // Tastendruck auswerten (keydown)
+
+    function keyhandler(key) {                                  // Tastatureingabe auswerten
         //console.log(key.keyCode);
         switch (key.keyCode) {
 
@@ -44,15 +100,21 @@ window.onload = function(){
 
             case 27:
                 console.log("ESC");
-                cmd.reset();
+                if (autofilLen > 0){                            // Autovervollständigung abbrechen und zurücksetzen, falls aktiv, ...
+                    input=typedinput;
+                    document.getElementById("cmdin").value = typedinput;
+                    autocompleteReset();
+                } else {
+                    cmd.reset();                                // ...ansonsten Eingabefeld löschen
+                }
                 break;
 
             case 8:
-                //e.preventDefault();
                 console.log("BACKDEL");
-                if (autofillen > 0) {
-                    document.getElementById("cmdin").value = input.slice(autofillen-1, input.length);
-                    autofillen = 0;
+                if (autofilLen > 0) {
+                    key.preventDefault();
+                    document.getElementById("cmdin").value = typedinput;
+                    autocompleteReset();
                 }
                 break;
 
@@ -62,7 +124,7 @@ window.onload = function(){
                 if (input.length > 0) {
                     if (shift) tab--;
                     else tab++;
-                    autocomplete(false);    // Autocomplete mit tab (ohne markierung)
+                    autocomplete(false);                 // Autocomplete mit Tabulator (ohne Markierung)
                 }
                 break;
 
@@ -74,6 +136,7 @@ window.onload = function(){
                     document.getElementById("cmdin").value = lastcommands[commandhistorypos];
                 }
                 commandhistorypos++;
+                console.log(commandhistorypos);
                 break;
 
             case 40:
@@ -89,46 +152,19 @@ window.onload = function(){
                 key.preventDefault();
                 console.log("ENTER");
                 readCmd();
+                autocompleteReset();
                 break;
 
             default:
                 if (input.length > 2) {
-                    autocomplete(true);    // Autocomplete bei Eingabe von mehr als 2 Buchstaben (mit Markierung)
+                    autocomplete(true);         // Autocomplete bei Eingabe von mehr als 2 Buchstaben (mit Markierung)
                 }
         }
     }
 
-    function shifthandler(key){
-        if(key.keyCode == 16){
-            shift = false;
-            console.log("SHIFT losgelassen");
-        }
-    }
+    // Funktionen für readCMD:
 
-    // Automatische Befehlsvervollständigung
-    function autocomplete(markierung){
-        //var cmdin = document.getElementById("cmdin");           // HTML-Object des Eingabefeldes
-        //var cmdinval = input;//document.getElementById("cmdin").value;  // Eingabefeld-Inhalt
-        var inputlow = input.toLowerCase();                        // Eingabefeld-Inhalt in Kleinbuchstaben
-        var inputlen = input.length;                                  // Textlänge des Eingabefeld-Inhalts
-        console.log("Autocomplete von " + inputlow);
-        for(let i=0; i<allcommands.length; i++) {                           // prüfe ob Text in Eingabefeld mit Anfang eines Befehls übereinstimmt
-            if (allcommands[i].startsWith(inputlow)) {
-                console.log(allcommands[i] + "-Befehl gefunden");
-                document.getElementById("cmdin").value = input + allcommands[i].slice(inputlen,allcommands[i].length); // ergänze den Befehl
-                if (markierung) cmdin.setSelectionRange(inputlen, allcommands[i].length, "backward");                                            // markiere die Ergänzung
-                autofillen = allcommands[i].length - inputlen;
-            }
-        }
-    }
-
-    // Focus auf das Eingabefeld setzen
-    function setFocus() {
-        document.getElementById("cmdin").focus();
-    }
-
-    // Befehl auf der Konsole "ausführen"
-    function append(ID) {
+    function append(ID) {                                  // Textbaustein auf die Konsole schreiben
         let newOrder = document.getElementById(ID).cloneNode(true);
         newOrder.style.display="block";
         newOrder.className="prompted";
@@ -138,8 +174,7 @@ window.onload = function(){
         cmd.scrollIntoView(false);
     }
 
-    // Befehl nach Eingabe in die Konsole schreiben
-    function appendCMD(cmd){
+    function appendCMD(cmd){                                // Befehl nach Eingabe in die Konsole schreiben
         let newOrder = document.getElementById("promptmessage").cloneNode(true);
         newOrder.style.display="block";
         newOrder.className="prompted";
@@ -150,8 +185,7 @@ window.onload = function(){
         document.getElementById("console").appendChild(newElement);
     }
 
-    // die Konsole leeren
-    function clear(){
+    function clear(){                                       // die Konsole leeren
         var con = document.getElementById("console");
         var toDelete = con.childNodes;
         for(var i=toDelete.length-1; i>=0; i--){
@@ -161,44 +195,56 @@ window.onload = function(){
         console.log("ausgabe gelöscht");
     }
 
-    // Eingabebefehl auswerten
+    function openURL(url){                                 // Öffnen der Seite in neuem Fenster
+        window.open(url);
+    }
+
+
+    // Eingabebefehl auswerten:
     function readCmd() {
-        command = input.toLowerCase();
-        input = "";
-        console.log(command);
-        appendCMD(command);
+
+        console.log(typedinput + " wurde eingegeben, " + input + " wurde verwendet.");
+        command = input.toLowerCase();                      // String aus Eingabfeld in Kleinbuchstaben zur weiteren Verarbeitung speichern
+        input = "";                                         // Eingabe-Variable und Eingabefeld leeren, sowie weitere Variablen zurück setzen...
+        cmd.reset();
+        tab = 0;
         commandhistorypos = 0;
-        if (linknamen.includes(command)) {                  // Links auswerten
-            console.log(command + "-Seite wird geöffnet");
+        appendCMD(command);                                 // Das eingegebene Kommando in das Konsolenfenster schreiben
+        if (linknamen.includes(command)) {                  // falls Eingabe ein Link, Link auswerten
             lastcommands.push(command);
             var linkindex = linknamen.indexOf(command);
-            console.log(linkindex);
-            window.open(linkelemente[linkindex].href);
+            console.log(command + "-Seite wird geöffnet. (Link-Index: " + linkindex + ")");
+            openURL(linkelemente[linkindex].href);
         } else {
-            if (commands.includes(command)) {               // Befehle auswerten
-                console.log("befehl vorhanden");
-                lastcommands.push(command);
-                if (command == "clear" || command == "cls") {
-                    clear();
-                } else if (command == "exit" || command == "quit") {
-                    clear();
-                    appendCMD(command);
-                    append(command);
-                    cmd.remove();
-                } else {
-                    append(command);
-                }
+            if (commands.includes(command)) {               // falls Eingabe ein Befehle, Befehl auswerten
+                console.log(command + "-befehl erkannt");
+                lastcommands.push(command);                 // Befehl in die Kommando-Historie aufnehmen
+                switch (command) {                          // cases für Funktionsbefehle, default für Textbausteine
+                    case "clear":
+                    case "cls":
+                        clear();
+                        break;
+                    case "exit":
+                    case "quit":
+                        clear();
+                        appendCMD(command);
+                        append(command);
+                        cmd.remove();
+                        break;
+                    case "commands":
 
+                    default:
+                        append(command);
+                        break;
+                }
             } else {
-                console.log("befehl nicht vorhanden");
+                console.log("befehl nicht vorhanden");      // Falls Eingabe unbekannt, fehler ausgeben
                 append("error");
             }
         }
-        cmd.reset();
-        tab = 0;
     }
 
     // Begrüßungsbotschaft beim laden in die Konsole schreiben:
     append("welcome");
-    setFocus(); // funktioniert nicht... :-(
-}
+    setFocus();
+});
